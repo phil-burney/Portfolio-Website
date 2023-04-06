01e14cb974a262c378796fc4b890465f5f0c4be1
Phil <peburney@ncsu.edu>
2023-04-05 23:42:01 -0400

    Fixed more complilation errors

--- src/Components/NavBar/NavLogo.tsx
+++ src/Components/NavBar/NavLogo.tsx
@@ -8,6 +8,7 @@ let style = {
 
 }
 
+// eslint-disable-next-line no-empty-pattern
 const Title: FC<NavLogoProps> = ({  }) => {
   return (
     <div className='d-flex justify-content-start px-3'><a href="/"><img style={style} className="img-flex p-1" src="../../logopb2.svg" alt=""></img></a></div>
